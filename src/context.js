68ea004e6a8bd2d509fc3a6695ec68b6b5feeda8
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-10-20 01:37:24 +0530

    SingleMovie Page setup & usefetch for api request

--- src/context.js
+++ src/context.js
@@ -1,59 +1,22 @@
-import React,{useContext,useEffect,useState } from 'react';
+import React, { useContext, useState } from "react";
+import useFetch from "./useFetch";
 
-const API_URL = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
+const AppContext = React.createContext();
 
-const AppContext =  React.createContext();
+// we are getting the children and that is app component in our case
+const AppProvider = ({ children }) => {
+  const [query, setQuery] = useState("hacker");
+  const { isLoading, isError, movie } = useFetch(`&s=${query}`);
 
-const AppProvider = ({children}) => {
-    const[isLoading,setIsLoading] = useState(true);
-    const[movie,setMovie] = useState([]);
-    const[isError, setIsError] = useState({show:"false",msg:""});
-    const[query,setQuery] = useState("titanic");
-    const getMovies = async(url)=>{
-        setIsLoading(true); 
-        try {
-            const res = await fetch(url);
-            const data = await res.json();
-            console.log(data);  
+  return (
+    <AppContext.Provider value={{ query, movie, setQuery, isLoading, isError }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
 
-            if(data.Response === "True"){
-                setIsLoading(false);
-                setIsError({
-                    show:false,
-                    msg :"",
-                })
-                setMovie(data.Search);
-            }
-            else{
-                setIsError({
-                    show:true,
-                    msg :data.Error,
-                })
+const useGlobalContext = () => {
+  return useContext(AppContext);
+};
 
-            }
-
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    
-
-    useEffect(()=>{
-
-  let timerOut = setTimeout(()=>{
-            getMovies(`${API_URL}a&s=${query}`);
-        },500);
-       
-        return()=>clearTimeout(timerOut);
-    },[query])
-
-    return <AppContext.Provider value={{ isLoading, isError, movie, query, setQuery }}> {children}</AppContext.Provider>;
-}
-
-const useGlobalContext = () =>{
- return useContext(AppContext);
-}
-
-
-export {AppContext,AppProvider,useGlobalContext};
+export { AppContext, AppProvider, useGlobalContext };
