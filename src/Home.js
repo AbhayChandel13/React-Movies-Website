ea8d1403727c04a61a10906f583c6a920da44d8e
Abhay Chandel <97330477+AbhayChandel13@users.noreply.github.com>
2022-12-12 20:02:08 +0530

    code format

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { useGlobalContext } from './context';
-import Movies from './Movies';
-import Search from './Search';
+import React from "react";
+import { useGlobalContext } from "./context";
+import Movies from "./Movies";
+import Search from "./Search";
 const Home = () => {
-    //const name = useContext(AppContext);
-    //const name = useGlobalContext();
+  //const name = useContext(AppContext);
+  //const name = useGlobalContext();
   return (
     <>
-     <div>
-    <Search />
-    <Movies />
-     </div>
-     {/* <p> {name}</p> */}
-     </>
-  )
-}
+      <div>
+        <Search />
+        <Movies />
+      </div>
+      {/* <p> {name}</p> */}
+    </>
+  );
+};
 
-export default Home
+export default Home;
