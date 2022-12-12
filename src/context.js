1a6388c522a460fd15fa79293fbd593b827ad015
Abhay Chandel <97330477+AbhayChandel13@users.noreply.github.com>
2022-12-12 20:02:24 +0530

    code format

--- src/context.js
+++ src/context.js
@@ -10,7 +10,9 @@ const AppProvider = ({ children }) => {
   const { isLoading, isError, movie } = useFetch(`&s=${query}&page=${page}`);
 
   return (
-    <AppContext.Provider value={{ query, movie, setQuery, isLoading, isError,page,setPage}}>
+    <AppContext.Provider
+      value={{ query, movie, setQuery, isLoading, isError, page, setPage }}
+    >
       {children}
     </AppContext.Provider>
   );
